16aaa6ca0ee98dc5b8d62c2758f5789818e63a10
bettyego <nwabethroseonuoha@gmail.com>
2025-06-13 17:24:28 +0100

    Fix case-sensitive import paths for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Component/Home";
-import MyWork from "./Component/Page/mywork";
-import Contact from "./Component/Page/contact"; 
-
-
-
+import MyWork from "./Component/page/MyWork";
+import Contact from "./Component/page/Contact";
 
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/mywork" element={<MyWork/>} />
-      <Route path="/contact" element={<Contact/>} />
-    
-    
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mywork" element={<MyWork />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 };
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from './Page/Header'
-import Hero from './Page/Hero'
-import Footer from './Page/Footer'
-import Contact from './Page/Contact'
+import Header from './page/Header'
+import Hero from './page/Hero'
+import Footer from './page/Footer'
+import Contact from './page/Contact'
 
 
 
