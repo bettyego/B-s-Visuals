a456921157e3129c101161a60f96839652250749
bettyego <nwabethroseonuoha@gmail.com>
2025-06-14 14:47:50 +0100

    update code

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import Header from './page/Header'
-import Hero from './page/Hero'
-import Footer from './page/Footer'
-import Contact from './page/Contact'
-
-
+import React from 'react';
+import Header from './page/Header';
+import Hero from './page/Hero';
+import Contact from './page/Contact';
+import Footer from './page/Footer';
 
 const Home = () => {
   return (
     <div>
-        <Header/>
-        <Hero/>
-        <Contact/>
-        <Footer/>
-
-        
-
+      <Header />
+      <Hero />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home ;
