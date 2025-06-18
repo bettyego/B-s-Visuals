5e323e2fa03c321f56891e6c63ed2151325a8097
bettyego <nwabethroseonuoha@gmail.com>
2025-06-18 17:54:37 +0100

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Body from "./component/Body";
 import MyWork from "./component/page/MyWork";
 import Contact from "./component/page/Contact";
+import Body from "./component/Body";
 
 const App = () => {
   return (
